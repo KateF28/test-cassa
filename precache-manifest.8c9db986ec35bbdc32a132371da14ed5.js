bdd3c18425acdeabbd4324fa256258898db7bdbf
Kate Fedorova <k_phedorova@ukr.net>
2020-03-15 02:46:06 +0200

    Updates

---
+++ precache-manifest.8c9db986ec35bbdc32a132371da14ed5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233ccf415095626f0bb9e9ebf81c251e",
+    "url": "/test-cassa/index.html"
+  },
+  {
+    "revision": "feff1d85f9f0a0c81e8e",
+    "url": "/test-cassa/static/css/2.7c3fc33d.chunk.css"
+  },
+  {
+    "revision": "3f64fb8aa3d5eadf853b",
+    "url": "/test-cassa/static/css/main.d71b95a7.chunk.css"
+  },
+  {
+    "revision": "feff1d85f9f0a0c81e8e",
+    "url": "/test-cassa/static/js/2.4080c80d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/test-cassa/static/js/2.4080c80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f64fb8aa3d5eadf853b",
+    "url": "/test-cassa/static/js/main.2b3165aa.chunk.js"
+  },
+  {
+    "revision": "a9bb50034fe36f7e498a",
+    "url": "/test-cassa/static/js/runtime-main.26435c0f.js"
+  },
+  {
+    "revision": "5a7ca73906f802910ffe31d7aed732df",
+    "url": "/test-cassa/static/media/Evogria.5a7ca739.woff"
+  },
+  {
+    "revision": "279172f86f81262c5373d610fad85d40",
+    "url": "/test-cassa/static/media/bg.279172f8.jpg"
+  },
+  {
+    "revision": "f7197f1637a02cc3bdeefa2d4498b2b4",
+    "url": "/test-cassa/static/media/doska@1X.f7197f16.png"
+  },
+  {
+    "revision": "8e7123855d877ada767c6c3853b0e692",
+    "url": "/test-cassa/static/media/product_apple@1X.8e712385.png"
+  },
+  {
+    "revision": "174f38d7e625714b5c99cc6ee22fad6e",
+    "url": "/test-cassa/static/media/product_berries@1X.174f38d7.png"
+  },
+  {
+    "revision": "3e2cec6effa2581138f43e03f3b9471c",
+    "url": "/test-cassa/static/media/product_grapefruit@1X.3e2cec6e.png"
+  },
+  {
+    "revision": "59ee810f2295924f96f3f6f4552a8330",
+    "url": "/test-cassa/static/media/product_pineapple@1X.59ee810f.png"
+  }
+]);
